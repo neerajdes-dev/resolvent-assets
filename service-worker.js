137b1a26df54c11fb24aa7ed0ecf9890740684df
Neeraj Desai <272821337+neerajdes-dev@users.noreply.github.com>
2026-07-15 09:37:46 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,1 @@
-const C='resolvent-2-3-0';self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const C='resolvent-2-3-1';self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
