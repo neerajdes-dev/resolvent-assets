f6151ccc26bebfb18193aa84759d1c8da1c6708c
Neeraj Desai <272821337+neerajdes-dev@users.noreply.github.com>
2026-07-17 17:17:06 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="resolvent-card-2-5-0";
+const CACHE="resolvent-card-2-5-1";
 const ASSETS=["./","./employee.html","./assets/css/style.css","./assets/js/app.js","./data/employees.json","./data/company.json","./data/services.json","./data/industries.json","./data/technologies.json","./data/engagement-models.json","./data/stats.json","./data/testimonials.json"];
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS))));
 self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))));
