6285175f9e4a9121e1669ea8b846037bec1ec44a
Neeraj Desai <272821337+neerajdes-dev@users.noreply.github.com>
2026-07-17 17:48:30 +0530

    Add clients.json to service worker cache assets

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const CACHE="resolvent-card-2-5-1";
-const ASSETS=["./","./employee.html","./assets/css/style.css","./assets/js/app.js","./data/employees.json","./data/company.json","./data/services.json","./data/industries.json","./data/technologies.json","./data/engagement-models.json","./data/stats.json","./data/testimonials.json"];
+const ASSETS=["./","./employee.html","./assets/css/style.css","./assets/js/app.js","./data/employees.json","./data/company.json","./data/services.json","./data/industries.json","./data/technologies.json","./data/engagement-models.json","./data/stats.json","./data/testimonials.json","./data/clients.json"];
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS))));
 self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))));
 self.addEventListener("fetch",event=>event.respondWith(fetch(event.request).catch(()=>caches.match(event.request))));
